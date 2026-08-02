90ebebe01aca80c729b2d00c35f612b267d468d1
midonawar24-svg <midonawar24@gmail.com>
2026-08-02 18:00:42 +0300

    Add files via upload

---
+++ lib/features/chat/models/message.dart
@@ -0,0 +1,10 @@
+enum MessageRole { user, assistant }
+
+class MessageModel {
+  final String id;
+  final String content;
+  final MessageRole role;
+  final DateTime timestamp;
+
+  MessageModel({required this.id, required this.content, required this.role, required this.timestamp});
+}
